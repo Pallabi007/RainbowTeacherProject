8d22b33e31c16dc3ea3906d88bf91b197aeb2799
Pallabi007 <pallabi.biswas@dell.com>
2021-10-29 21:18:52 +0530

    Add project files.

---
+++ TeachersDataAccess/TeacherDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeacherDataStore
+{
+    class TeacherDetails
+    {       
+        public int ID { get; set; }
+        public string  Name { get; set; }
+        public int Class { get; set; }
+        public string Section { get; set; }
+    }
+}
